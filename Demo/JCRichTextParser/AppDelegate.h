a31415d21e6bf05b78a13b149a68081a3dd80882
Jimmy <wmywbyt.cj@gmail.com>
2016-01-14 18:11:14 +0800

    init project

---
+++ Demo/JCRichTextParser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JCRichTextParser
+//
+//  Created by Jimmy Chan on 1/14/16.
+//  Copyright © 2016 Jimmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/JCRichTextParser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JCRichTextParser
+//
+//  Created by Jimmy Chan on 1/14/16.
+//  Copyright © 2016 Jimmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
