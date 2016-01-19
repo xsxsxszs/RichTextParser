be65cd3ddb846ed4512ad68304359eabc38e24f3
Jimmy <wmywbyt.cj@gmail.com>
2016-01-19 15:24:30 +0800

    demo

--- Demo/JCRichTextParser/ViewController.m
+++ Demo/JCRichTextParser/ViewController.m
@@ -6,8 +6,13 @@
 //
 
 #import "ViewController.h"
+#import "JCRichTextParser.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) NSArray *testCases;
+@property (nonatomic, strong) JCRichTextParser *defaultParser;
+@property (nonatomic, strong) JCRichTextParser *customizedParser;
 
 @end
 
@@ -21,6 +26,55 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    //init test cases
+    self.testCases = @[
+                       @"A <b>bold</b> and <i>italic</i> text",
+                       @"A only <b><i>bold</i></b> text",
+                       @"A <italic_green>italic green</italic_green> and <large_bold>large bold</large_bold> text",
+                       ];
+    
+    self.defaultParser = [JCRichTextParser defaultConvertorWithFontSize:17.0];
+    
+    JCRichTextTag *tag1 = [[JCRichTextTag alloc] initWithStartTag:@"<italic_green>" endTag:@"</italic_green>" attributes:@{NSFontAttributeName: [UIFont italicSystemFontOfSize:17.0], NSForegroundColorAttributeName: [UIColor greenColor]}];
+    JCRichTextTag *tag2 = [[JCRichTextTag alloc] initWithStartTag:@"<large_bold>" endTag:@"</large_bold>" attributes:@{NSFontAttributeName: [UIFont boldSystemFontOfSize:34.0]}];
+    self.customizedParser = [[JCRichTextParser alloc] initWithTags:@[tag1, tag2]];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return self.testCases.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell)
+    {
+        cell = [[UITableViewCell alloc] init];
+        cell.textLabel.numberOfLines = 0;
+    }
+    if (indexPath.row == 0)
+    {
+        cell.textLabel.text = self.testCases[indexPath.section];
+    }
+    else if (indexPath.row == 1)
+    {
+        if (indexPath.section == self.testCases.count -1)
+        {
+            cell.textLabel.attributedText = [self.customizedParser parseWithText:self.testCases[indexPath.section]];
+        }
+        else
+        {
+            cell.textLabel.attributedText = [self.defaultParser parseWithText:self.testCases[indexPath.section]];
+        }
+    }
+    return cell;
 }
 
 @end
